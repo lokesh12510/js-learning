04bd8eb82e4f467549364f76c3f80c8f0dbdd6fc
lokeshk <lokesh.krishnamoorthy@intergy.com.au>
2025-01-25 22:13:42 +0530

    Promises

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const api = "https://jsonplaceholder.typicode.com/posts";
+
+const promise1 = new Promise((resolve, reject) => {
+	console.log("promise1 called");
+	setTimeout(() => {
+		resolve(api);
+	}, 10000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+	console.log("promise2 called");
+	setTimeout(() => {
+		resolve("Promise2 is resolved");
+	}, 5000);
+});
+
+const getData = async () => {
+	console.log("Calling the promise");
+	const data = await promise1;
+	console.log(data);
+
+	const data2 = await fetch(data);
+	console.log(data2);
+
+	console.log("Finished");
+};
+
+getData();
+console.log("Promise called");
 // function z() {
 //   function x() {
 //     let a = 10;
@@ -10,15 +39,26 @@
 //   console.log(b);
 // }
 // z();
-function z() {
-  function x() {
-    return function y() {
-      console.log("fn");
-    };
-  }
+// function z() {
+//   function x() {
+//     return function y() {
+//       console.log("fn");
+//     };
+//   }
 
-  let b = x();
+//   let b = x();
 
-  console.log(b());
+//   console.log(b());
+// }
+// z();
+
+function clickCount(id) {
+	let count = 0;
+
+	document.getElementById(id).addEventListener("click", function () {
+		console.log(`Button clicked ${id} ${++count}`);
+	});
 }
-z();
+
+clickCount("clickMe");
+clickCount("clickMe2");
